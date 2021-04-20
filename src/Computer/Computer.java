07fff586e4dfa7dc5b0f56621ac4118f0c143771
UG_Oscar Johansson <os3111jo-s@student.lu.se>
2021-04-20 18:32:08 +0200

    Update Computer.java

--- src/Computer/Computer.java
+++ src/Computer/Computer.java
@@ -1,5 +1,19 @@
 package Computer;
 
+import Program.Program;
+
 public class Computer {
-	
+
+    private Memory memory;
+    private Program program;
+
+	public Computer(Memory memory) {
+        this.memory = memory;
+    }
+    public void load(Program program){
+        this.program = program;
+    }
+    public void run(){
+        
+    }
 }
