128b2e0ae77e27404491f717b36833bcb02f067b
Alexandra <alexandragalonja@gmail.com>
2021-04-21 17:09:27 +0200

    Update Mul.java

--- src/Program/Mul.java
+++ src/Program/Mul.java
@@ -1,5 +1,25 @@
 package Program;
 
-public class Mul {
+import Computer.Word;
+
+public class Mul extends BinaryExpr {
+	
+	public Mul(Operand o1, Operand o2, Address address, String opName) {
+		super(o1, o2, address);
+		
+	}
+
+	@Override
+	protected Word compute(Word o1, Word o2) {
+		Word newWord = o1.mul(o2);
+		
+		return newWord;
+	}
+
+	@Override
+	public String toString() {
+		return o1 + " " + o2 + " " + address;
+	}
+
 
 }
