d9889f5b21717c78db2515a8c13ea1fd8bc6424f
Alexandra <alexandragalonja@gmail.com>
2021-04-21 17:35:33 +0200

    Updated Instruction.java, deleted unused import and swapped places of the two methods.

--- src/Program/Instruction.java
+++ src/Program/Instruction.java
@@ -2,15 +2,12 @@ package Program;
 
 import Computer.Memory;
 import Computer.PC;
-import Computer.Word;
 
 public abstract class Instruction {
 	
+	public abstract void execute(Memory mem, PC pc);
 	
 	public abstract String toString();
-
-
-	public abstract void execute(Memory mem, PC pc);
 
 
 	
