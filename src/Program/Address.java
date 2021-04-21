266d139f15e0d2aa71b1a4b6e1d93d370616e9c3
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-04-21 10:18:52 +0200

    Operand is now an interface, added methods to Address

--- src/Program/Address.java
+++ src/Program/Address.java
@@ -1,5 +1,17 @@
 package Program;
 
-public class Address {
+import Computer.Memory;
+import Computer.Word;
 
+public class Address implements Operand{
+	private int address;
+	
+	public Address(int index) {
+		
+	}
+	
+	public Word getWord(Memory mem) {
+		
+		return null;
+	}
 }
--- src/Program/Operand.java
+++ src/Program/Operand.java
@@ -1,5 +1,5 @@
 package Program;
 
-public abstract class Operand {
-
+public interface Operand {
+	
 }
