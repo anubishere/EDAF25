c0b03d0fee16bdc62d999afbe1414c73cf4ae94f
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-04-21 10:27:03 +0200

    Added implementation of fetching words from memory using Address

--- src/Computer/Memory.java
+++ src/Computer/Memory.java
@@ -1,10 +1,16 @@
 package Computer;
 
+import Program.Address;
+
 public abstract class Memory {
 	private Word[] memory;
 	
 	public Memory(int length) {
 		this.memory = new Word[length];
+	}
+	
+	public Word read(Address a) {
+		return this.memory[a.getIndex()];
 	}
 
 }
--- src/Program/Address.java
+++ src/Program/Address.java
@@ -4,14 +4,19 @@ import Computer.Memory;
 import Computer.Word;
 
 public class Address implements Operand{
-	private int address;
+	private int index;
 	
 	public Address(int index) {
-		
+		this.index = index;
+	}
+	
+	public int getIndex() {
+		return this.index;
 	}
 	
 	public Word getWord(Memory mem) {
-		
-		return null;
+		return mem.read(this);
 	}
+	
+	
 }
