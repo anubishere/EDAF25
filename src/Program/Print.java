004ee0feb432203d98c5ee2cbaeaf505ef48080a
Alexandra <alexandragalonja@gmail.com>
2021-04-21 17:36:31 +0200

    Update Print.java

--- src/Program/Print.java
+++ src/Program/Print.java
@@ -1,5 +1,30 @@
 package Program;
 
-public class Print {
+import Computer.Memory;
+import Computer.PC;
+
+public class Print extends Instruction {
+	
+	private String opName;
+	private Operand op;
+	
+	public Print(String opName, Operand op) {
+		this.opName = opName;
+		this.op = op;
+		
+	}
+	
+	@Override
+	public void execute(Memory mem, PC pc) {
+		System.out.println(op.readOperand(mem));
+		pc.setPC(pc.getPC() + 1);
+		
+	}
+
+	@Override
+	public String toString() {
+		return opName + " " + op;
+		
+	}
 
 }
