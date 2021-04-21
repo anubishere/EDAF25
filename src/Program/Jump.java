1815b65e2b2e0e1d462508f039f43474880c965f
Alexandra <alexandragalonja@gmail.com>
2021-04-21 17:20:18 +0200

    Update Jump.java

--- src/Program/Jump.java
+++ src/Program/Jump.java
@@ -1,5 +1,31 @@
 package Program;
 
-public class Jump {
+import Computer.Memory;
+import Computer.PC;
 
+public class Jump extends Instruction {
+	
+	private final String opName;
+	private int dest;
+	private Operand op;
+	
+	public Jump(String opName, int dest, Operand op) {
+		this.opName = opName;
+		this.dest = dest;
+		this.op = op;
+		
+	}
+	
+	@Override
+	public void execute(Memory mem, PC pc) {
+		pc.setPC(pc.getPC() + dest);
+		
+	}
+
+	@Override
+	public String toString() {
+		return opName + " " + op;
+	}
+
+	
 }
