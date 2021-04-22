e664c0882462c79b3d816c0e26f26770199c86f7
UG_Oscar Johansson <os3111jo-s@student.lu.se>
2021-04-22 14:55:05 +0200

    Update LongMemory.java

--- src/Computer/LongMemory.java
+++ src/Computer/LongMemory.java
@@ -5,10 +5,4 @@ public class LongMemory extends Memory{
     public LongMemory(int size) {
         super(size);
     }
-    
-    protected void populate(int size) {
-    	for(int i = 0; i < size; i++) {
-    		this.memory[i] = new LongWord(0);
-    	}
-    }
 }
