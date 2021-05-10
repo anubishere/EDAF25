4d497386cd4652897e4a190ee39cc9e89cdaacf8
Alexandra <alexandragalonja@gmail.com>
2021-05-10 10:47:25 +0200

    Update CircularCell.java

--- XL/src/main/java/model/CircularCell.java
+++ XL/src/main/java/model/CircularCell.java
@@ -1,4 +1,14 @@
 package model;
 
-public class CircularCell {
+import expr.ErrorResult;
+import expr.ExprResult;
+import expr.Environment;
+
+public class CircularCell implements CellEntry  {
+
+    @Override public ExprResult value(Environment e) {
+        throw new Error("Circular Value");
+
+    }
+
 }
