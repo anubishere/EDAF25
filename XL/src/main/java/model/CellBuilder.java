b7e64a8578ba4321468e2e70ff2f9bc0cdbe4da3
SebastianForslund <sebastian.david.forslund@gmail.com>
2021-05-10 14:03:58 +0200

    Added a static method stringToAddress that converts String addresses to CellAddress ones

--- XL/src/main/java/model/CellBuilder.java
+++ XL/src/main/java/model/CellBuilder.java
@@ -18,4 +18,14 @@ public class CellBuilder {
             return new ExpressionCell(ex.build(text));
         }
     }
+
+    public static CellAddress stringToAddress(String address) {
+        char firstChar = ((char) address.charAt(0));
+        int col = (int)firstChar - 65;
+        String row = "";
+        for (int i=1; i<address.length(); i++) {
+            row += address.charAt(i);
+        }
+        return new CellAddress(col, Integer.parseInt(row));
+    }
 }
